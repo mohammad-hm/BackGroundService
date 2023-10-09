58a7537ce903a3dd24eb015ada61051fbd65f171
mohammad hajimohammadi <hajimohammadi@amnpardaz.com>
2023-10-09 14:03:57 +0330

    Update RandomNumberGeneratorBackgroundService.cs

--- BackGroundDotService/RandomNumberGeneratorBackgroundService.cs
+++ BackGroundDotService/RandomNumberGeneratorBackgroundService.cs
@@ -13,10 +13,12 @@ namespace BackGroundDotService
     {
         private readonly ILogger<RandomNumberGeneratorBackgroundService> _logger;
         private Timer? _timer;
+        private int _executionCount;
 
         public RandomNumberGeneratorBackgroundService(ILogger<RandomNumberGeneratorBackgroundService> logger)
         {
             _logger = logger;
+            _executionCount = 0;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -31,12 +33,19 @@ namespace BackGroundDotService
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("RandomNumberGeneratorBackgroundService is stopping.");
+            _logger.LogInformation("TimerBackgroundService is stopping.");
 
             // Stop the timer
             _timer?.Change(Timeout.Infinite, 0);
 
             return Task.CompletedTask;
+        }
+
+        // Custom method to stop the background service
+        public void Stop()
+        {
+            _logger.LogInformation("Stopping TimerBackgroundService manually.");
+            StopAsync(CancellationToken.None).Wait(); // You may want to use a more graceful way to stop it
         }
 
         public void Dispose()
@@ -48,6 +57,14 @@ namespace BackGroundDotService
         {
             var randomNumber = new Random().Next(1, 1000);
             _logger.LogInformation($"Generated random number: {randomNumber}");
+            // Check your custom condition here
+            if (_executionCount >= 5)
+            {
+                Stop();
+            }
+
+            _executionCount++;
+
         }
     }
 
